e99479a066f471fd86f33ad1c41b80c90d08e832
DeedleFake <deedlefake@users.noreply.github.com>
2015-09-02 14:45:21 -0400

    More basic implementation.

--- pkglines.go
+++ pkglines.go
@@ -3,8 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go/build"
 	"os"
 )
+
+type Package struct {
+	Name  string
+	Lines int
+}
+
+func countLines(linesC chan Package, pkg *build.Package) {
+}
 
 func main() {
 	flag.Usage = func() {
@@ -12,4 +21,27 @@ func main() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	linesC := make(chan Package, flag.NArg())
+
+	done := make(map[string]struct{}, flag.NArg())
+	for _, ipath := range flag.Args() {
+		if _, ok := done[ipath]; ok {
+			continue
+		}
+		done[ipath] = struct{}{}
+
+		pkg, err := build.Import(ipath, ".", 0)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to import %q: %v", ipath, err)
+			os.Exit(1)
+		}
+
+		go countLines(linesC, pkg)
+	}
 }
